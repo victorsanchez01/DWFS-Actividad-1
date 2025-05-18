fe8c87c8ca3a6d5ff5d121027f3fdede0a1cf6b5
Victor Sanchez <vs@victorsanchez.dev>
2025-05-18 18:59:09 -0500

    Fixed path on layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../Header/Header'
-import Footer from "../Footer/Footer.jsx";
+import Footer from '../Footer/Footer'
 import './Layout.css'
 
 function Layout({ children }) {
