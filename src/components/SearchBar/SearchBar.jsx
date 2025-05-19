38cc2237e480a52b4a56d7cb27ec5ab5e8055b14
Victor Sanchez <vs@victorsanchez.dev>
2025-05-18 19:13:39 -0500

    Refactored SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,24 +5,19 @@ import './SearchBar.css'
 function SearchBar({ onSearch }) {
     const [searchTerm, setSearchTerm] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSearch(searchTerm);
-    };
-
-    const handleChange = (e) => {
+    const handleSearch = (e) => {
         setSearchTerm(e.target.value);
         onSearch(e.target.value);
     };
 
     return (
-        <form className="search-bar" onSubmit={handleSubmit}>
+        <form className="search-bar">
             <input
                 type="text"
                 className="search-bar__input"
                 placeholder="Buscar por título de libro..."
                 value={searchTerm}
-                onChange={handleChange}
+                onChange={handleSearch}
             />
             <div className="search-bar__icon">
                 <SearchBarIcon />
